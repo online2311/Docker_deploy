47ae0b31e916269a0fff0a86e9246cde1a846049
Colette Contreras <ColetteContreras@outlook.com>
2020-10-22 17:14:27 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,12 +4,8 @@ chmod a+x /usr/local/bin/docker-compose
 rm -rf `which dc`
 ln -s /usr/local/bin/docker-compose /usr/bin/dc
 
-echo -n "请输入安装目录（默认/www/wwwroot/proxypanel）: "
-read install_dir
-idir=${install_dir:-"/www/wwwroot/proxypanel"}
-
-mkdir -p $idir && cd $idir
-wget https://raw.githubusercontent.com/ColetteContreras/proxypanel-docker/main/docker-compose.yml
-wget https://raw.githubusercontent.com/ColetteContreras/proxypanel-docker/main/env
+mkdir -p ${install_dir} && cd ${install_dir}
+curl -fsSL https://raw.githubusercontent.com/ColetteContreras/proxypanel-docker/main/docker-compose.yml -o docker-compose.yml
+curl -fsSL https://raw.githubusercontent.com/ColetteContreras/proxypanel-docker/main/env -o env
 sed -i "s|APP_KEY=placeholder|APP_KEY=base64:$(openssl rand -base64 32)|" env
 dc pull && dc up -d
