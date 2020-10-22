a4d7cd698f7a07c599eb0db5d651fa04c38647af
Colette Contreras <ColetteContreras@outlook.com>
2020-10-22 22:28:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -25,5 +25,14 @@ ln -s /usr/local/bin/docker-compose /usr/bin/dc
 mkdir -p ${install_dir} && cd ${install_dir}
 curl -fsSL https://raw.githubusercontent.com/ColetteContreras/proxypanel-docker/main/docker-compose.yml -o docker-compose.yml
 curl -fsSL https://raw.githubusercontent.com/ColetteContreras/proxypanel-docker/main/env -o env
+
+mysqlPW="$(openssl rand -hex 12)"
+
 sed -i "s|APP_KEY=placeholder|APP_KEY=base64:$(openssl rand -base64 32)|" env
+sed -i "s|MYSQL_ROOT_PASSWORD=proxypanel|MYSQL_ROOT_PASSWORD=${mysqlPW}|" env
+sed -i "s|DB_PASSWORD=proxypanel|DB_PASSWORD=${mysqlPW}|" env
+
 dc pull && dc up -d
+
+echo "MySQL root password: ${mysqlPW}"
+echo "Show logs: cd ${install_dir} && dc logs -f web"
