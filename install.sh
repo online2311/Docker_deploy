129504f7b866ab12c1006a3a276197bf72bf8054
ZhangJing <13821320100@outlook.com>
2022-05-15 07:41:57 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -32,7 +32,7 @@ sed -i "s|APP_KEY=placeholder|APP_KEY=base64:$(openssl rand -base64 32)|" env
 sed -i "s|MYSQL_ROOT_PASSWORD=proxypanel|MYSQL_ROOT_PASSWORD=${mysqlPW}|" env
 sed -i "s|DB_PASSWORD=proxypanel|DB_PASSWORD=${mysqlPW}|" env
 
-dc pull && dc up -d && dc exec -it web php artisan migrate --seed --force
+dc pull && dc up -d && sleep 10 && dc exec -it web php artisan migrate --seed --force
 
 echo "MySQL root password: ${mysqlPW}"
 echo "Show logs: cd ${install_dir} && dc logs -f web"
