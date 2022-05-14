34065359d7d9a0679e29ca2c41bac9c22f572b2c
ZhangJing <13821320100@outlook.com>
2022-05-15 07:39:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,8 +23,8 @@ rm -rf `which dc`
 ln -s /usr/local/bin/docker-compose /usr/bin/dc
 
 mkdir -p ${install_dir} && cd ${install_dir}
-curl -fsSL https://raw.githubusercontent.com/proxypanel/Docker_deploy/main/docker-compose.yml -o docker-compose.yml
-curl -fsSL https://raw.githubusercontent.com/proxypanel/Docker_deploy/main/env -o env
+curl -fsSL https://raw.githubusercontent.com/online2311/Docker_deploy/main/docker-compose.yml -o docker-compose.yml
+curl -fsSL https://raw.githubusercontent.com/online2311/Docker_deploy/main/env -o env
 
 mysqlPW="$(openssl rand -hex 12)"
 
@@ -32,7 +32,7 @@ sed -i "s|APP_KEY=placeholder|APP_KEY=base64:$(openssl rand -base64 32)|" env
 sed -i "s|MYSQL_ROOT_PASSWORD=proxypanel|MYSQL_ROOT_PASSWORD=${mysqlPW}|" env
 sed -i "s|DB_PASSWORD=proxypanel|DB_PASSWORD=${mysqlPW}|" env
 
-dc pull && dc up -d
+dc pull && dc up -d && dc exec -it web php artisan migrate --seed --force
 
 echo "MySQL root password: ${mysqlPW}"
 echo "Show logs: cd ${install_dir} && dc logs -f web"
